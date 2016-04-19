adc83628ccc6c0d9b93d4509a5085487eb94bfd1
Jérémie Veillet <jeremie.veillet@gmail.com>
2016-04-19 11:20:55 +0200

    Improve styleguide
    Changed buttons CSS rules so that default browser appearences
    are not taken into account.

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
     <!-- main -->
 	<main role="main" class="main-container">
 
-
 		<!-- section -->
 		<section>
 
 		  <?php //get_template_part('searchform'); ?>
 
+      <button onClick="scrollTo(0, 500);">click to scroll down 100 pixels</button>
+
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
+
 
 		</section><!-- end of section -->
 
