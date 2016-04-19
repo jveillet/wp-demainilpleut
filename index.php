2e37a5df2f566eb8f3209ed87a537ade87245c78
Jérémie Veillet <jeremie.veillet@gmail.com>
2016-04-19 17:16:16 +0200

    Change time icon colors
    Colors in the SCVG file updated.
    Strokes harder to better see the icon on non
    HDPI screens.
    Changed fallback icon color too.

--- index.php
+++ index.php
@@ -7,16 +7,12 @@
 
 		  <?php //get_template_part('searchform'); ?>
 
-      <button onClick="scrollTo(0, 500);">click to scroll down 100 pixels</button>
-
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
-
 
 		</section><!-- end of section -->
 
 	</main><!-- end of main -->
 
 <?php get_footer(); ?>
-
